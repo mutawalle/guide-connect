e8ae5730287f79594510595c77fd7e060882d827
Fajar Herawan <13521080@std.stei.itb.ac.id>
2023-06-27 23:28:47 +0700

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,15 +3,20 @@ import React, { useState,useEffect,useRef } from 'react'
 const Sidebar = () => {
     const [isOpen, setIsOpen] = useState(false);
     const sidebarRef = useRef<HTMLBaseElement>(null);
+    const buttonRef = useRef<HTMLButtonElement>(null);
 
   const toggleSidebar = () => {
-    setIsOpen(!isOpen);
+    setIsOpen(true);
   };
 
   const handleClickOutside = (event: MouseEvent) => {
-    if (sidebarRef.current && !sidebarRef.current.contains(event.target as Node)) {
-      setIsOpen(!isOpen);
+    if (sidebarRef.current && !buttonRef.current && !sidebarRef.current.contains(event.target as Node)) {
+      setIsOpen(false);
       console.log('click outside');
+      console.log(isOpen)
+    }else if(buttonRef.current){
+        console.log('click inside');
+        console.log(isOpen)
     }
   };
 
@@ -24,7 +29,7 @@ const Sidebar = () => {
   }, []);
   return (
     <>
-        <button id="sidebarbutton" type="button" className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" onClick={toggleSidebar}>
+        <button ref={buttonRef} id="sidebarbutton" type="button" className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" onClick={toggleSidebar}>
             <span className="sr-only">Open sidebar</span>
             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
