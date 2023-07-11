bd53629f10ccede850a81e409e94f0c8870f5533
Fajar Herawan <13521080@std.stei.itb.ac.id>
2023-07-11 22:17:19 +0700

    login

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import Form from './components/Form'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='bg-white h-screen w-screen flex justify-center items-center'>
+        <Form/>
+    </div>
   )
 }
 
